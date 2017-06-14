7e413625d95f1bed5007d87758716f56ea942aa9
Rizwan Aarif <rizwan23arif@gmail.com>
2017-06-14 10:48:11 -0300

    Create Arrays & Stacks.java

--- Arrays & Stacks.java
+++ Arrays & Stacks.java
@@ -44,32 +44,25 @@ EXPLANATION: Given a string containing just the characters '(', ')', '{', '}', '
 public class Solution {
     public boolean isValid(String s) {
         
-        if(s.length() == 0)
+        if (s.length() == 0)
             return true;
-        
+            
         Stack<Character> stack = new Stack<>();
-        
-        for(int i=0; i<s.length(); i++){
             
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
-                stack.push(s.charAt(i));
-                continue;
-            }
-            
-            
-            if(stack.empty() && (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']'))
-                return false;
-                
-            if((s.charAt(i) == ')'  && stack.peek() == '(') || 
-               (s.charAt(i) == '}'  && stack.peek() == '{') || 
-               (s.charAt(i) == ']'  && stack.peek() == '['))
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) == '(')
+                stack.push(')');
+            else if (s.charAt(i) == '{')
+                stack.push('}');
+            else if (s.charAt(i) == '[')
+                stack.push(']');
+            else if (!stack.empty() && s.charAt(i) == stack.peek())
                 stack.pop();
             else
                 return false;
-
         }
         
-        return (stack.empty());
+        return stack.empty();
         
     }
 }

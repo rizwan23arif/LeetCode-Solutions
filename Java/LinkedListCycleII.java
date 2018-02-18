92288c8deaef95ceaca8e2fc29f84892ab003296
Rizwan Aarif <rizwan23arif@gmail.com>
2018-02-18 10:47:34 -0500

    Update LinkedListCycleII.java

--- Java/LinkedListCycleII.java
+++ Java/LinkedListCycleII.java
@@ -10,18 +10,18 @@ Can you solve it without using extra space?
 
 public class LinkedListCycleII {
         public ListNode detectCycle(ListNode head) {
-          ListNode slow = head, fast = head;
-          while(fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (slow == fast) {
-              while (head != slow) {
-                head = head.next;
-                slow = slow.next;
-              }
-              return slow;				
-            }
-          }			
-          return null;
+                ListNode slow = head, fast = head;
+                while(fast != null && fast.next != null) {
+                        fast = fast.next.next;
+                        slow = slow.next;
+                        if (slow == fast) {
+                                while (head != slow) {
+                                        head = head.next;
+                                        slow = slow.next;
+                                }
+                                return slow;				
+                        }
+                }			
+                return null;
         }
 }
